384383bb77a3891f541d49f66f0ac7060befc759
dianabotnaru <dianabotnaru101@gmail.com>
2017-09-28 04:02:21 +0200

    first commit for smart gel

---
+++ SmartGel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SmartGel
+//
+//  Created by jordi on 28/09/2017.
+//  Copyright © 2017 AFCO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SmartGel/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SmartGel
+//
+//  Created by jordi on 28/09/2017.
+//  Copyright © 2017 AFCO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
