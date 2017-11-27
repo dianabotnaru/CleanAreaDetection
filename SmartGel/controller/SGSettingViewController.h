65eb87edbffe5b0ecef119c18bc44a8dc3aff665
dianabotnaru <dianabotnaru101@gmail.com>
2017-11-27 13:11:33 +0100

    added SGSettingViewController

---
+++ SmartGel/controller/SGSettingViewController.h
@@ -0,0 +1,13 @@
+//
+//  SGSettingViewController.h
+//  SmartGel
+//
+//  Created by jordi on 27/11/2017.
+//  Copyright © 2017 AFCO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SGSettingViewController : UIView
+
+@end
---
+++ SmartGel/controller/SGSettingViewController.m
@@ -0,0 +1,21 @@
+//
+//  SGSettingViewController.m
+//  SmartGel
+//
+//  Created by jordi on 27/11/2017.
+//  Copyright © 2017 AFCO. All rights reserved.
+//
+
+#import "SGSettingViewController.h"
+
+@implementation SGSettingViewController
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
