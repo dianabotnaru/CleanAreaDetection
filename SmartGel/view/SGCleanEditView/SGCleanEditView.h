b2beecad8ed57f89ccee16f39c28a32d3b168d5c
dianabotnaru <dianabotnaru101@gmail.com>
2018-02-02 15:11:22 +0100

    added SGCleanEditView

---
+++ SmartGel/view/SGCleanEditView/SGCleanEditView.h
@@ -0,0 +1,15 @@
+//
+//  SGCleanEditView.h
+//  SmartGel
+//
+//  Created by jordi on 02/02/2018.
+//  Copyright © 2018 AFCO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SGCleanEditView : UIView
+
+
+
+@end
---
+++ SmartGel/view/SGCleanEditView/SGCleanEditView.m
@@ -0,0 +1,16 @@
+//
+//  SGCleanEditView.m
+//  SmartGel
+//
+//  Created by jordi on 02/02/2018.
+//  Copyright © 2018 AFCO. All rights reserved.
+//
+
+#import "SGCleanEditView.h"
+
+@implementation SGCleanEditView
+
+- (void)drawRect:(CGRect)rect {
+}
+
+@end
